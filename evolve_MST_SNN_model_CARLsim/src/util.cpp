c54ff773363afb77f67dcbecac2e6dd350f53a41
Robert Bain <31903812+bainro@users.noreply.github.com>
2022-08-07 20:18:28 -0700

    still...

--- evolve_MST_SNN_model_CARLsim/src/util.cpp
+++ evolve_MST_SNN_model_CARLsim/src/util.cpp
@@ -91,7 +91,6 @@ float** loadData(string file, int numRow, int numCol) {
                 getline(s, field, ',');
                 istringstream str(field);
                 str >> out_data[i][j];
-		    cout << out_data[i][j] << "\n";	
             }
         }
     }
