506f4de13023c6f6cbb8509e890a6305953e0e20
Robert Bain <31903812+bainro@users.noreply.github.com>
2022-08-11 20:26:12 -0700

    adding matplotlib quiver plot for vec field of OF

--- vis_pfm.py
+++ vis_pfm.py
@@ -2,8 +2,8 @@
 
 import re
 import numpy as np
-import sys
- 
+import sysimport matplotlib.pyplot as plt
+
 
 def readPFM(file):
     file = open(file, 'rb')
@@ -70,3 +70,33 @@ def writePFM(file, image, scale=1):
     file.write('%f\n' % scale)
 
     image.tofile(file)
+    
+vis_test():
+    # Creating arrow
+    x = np.arange(0, 2 * np.pi + 2 * np.pi / 20,
+                  2 * np.pi / 20)
+    y = np.arange(0, 2 * np.pi + 2 * np.pi / 20,
+                  2 * np.pi / 20)
+
+    X, Y = np.meshgrid(x, y)
+
+    u = np.sin(X)*np.cos(Y)
+    v = -np.cos(X)*np.sin(Y)
+
+    # Defining color
+    color = np.sqrt(((dx-n)/2)*2 + ((dy-n)/2)*2)
+
+    # Creating plot
+    fig, ax = plt.subplots(figsize =(14, 9))
+    ax.quiver(X, Y, u, v, color, alpha = 1)
+
+    ax.xaxis.set_ticks([])
+    ax.yaxis.set_ticks([])
+    ax.axis([0, 2 * np.pi, 0, 2 * np.pi])
+    ax.set_aspect('equal')
+
+    # show plot
+    plt.show()
+    
+if __name__ == "__main__":
+    vis_test()
