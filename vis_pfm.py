90cf8330fe7b84fd4df4969699b4eb6afebfcd71
Robert Bain <31903812+bainro@users.noreply.github.com>
2022-08-15 19:27:23 -0700

    Update vis_pfm.py

--- vis_pfm.py
+++ vis_pfm.py
@@ -99,7 +99,7 @@ def flow_img(file="test.pfm"):
     rgb = cv2.resize(rgb, dsize=flow_dims, interpolation=cv2.INTER_CUBIC)
     
     # Creating plot
-    fig, axes = plt.subplots(1, 3, figsize=(20, 6))
+    fig, axes = plt.subplots(1, 3, figsize=(22, 6))
     axes[0].imshow(rgb)
     
     flow, _ = readPFM(file)
@@ -116,6 +116,7 @@ def flow_img(file="test.pfm"):
     
     a = np.sqrt((u ** 2) * (v ** 2))
     a = np.log(a + 1)
+    axes[1].axis('off')
     axes[1].imshow(a)
     
     h, w = flow.shape[:2]
@@ -145,6 +146,7 @@ def flow_img(file="test.pfm"):
     # Defining color
     color = np.sqrt(u**2 + v**2).flatten()
 
+    axes[2].axis('off')
     axes[2].quiver(X, Y, u, v, color)
     # trying to make top-left pt 0,0
     axes[2].invert_yaxis()
