80470d22d412a295a1800b0cdf09541e594f0d23
Robert Bain <31903812+bainro@users.noreply.github.com>
2022-08-15 19:49:06 -0700

    Update vis_pfm.py

--- vis_pfm.py
+++ vis_pfm.py
@@ -171,7 +171,7 @@ def make_flow_mp4(load_dir="./driving", fps=10, v_name="test.mp4"):
                 break    
             
     w, h, _c = frames[0].shape
-    fourcc = cv2.VideoWriter_fourcc('a','v','c','1')
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
     writer = cv2.VideoWriter(v_name, fourcc, fps, (w, h))
 
     for frame in frames:
