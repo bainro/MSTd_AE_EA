8dd4277ef8fca31bfdb76bb146c60614514f5554
Robert Bain <31903812+bainro@users.noreply.github.com>
2022-08-15 19:41:10 -0700

    typo*: cv -> cv2

--- vis_pfm.py
+++ vis_pfm.py
@@ -171,8 +171,8 @@ def make_flow_mp4(load_dir="./driving", fps=10, v_name="test.mp4"):
                 break    
             
     w, h, _c = frames[0].shape
-    fourcc = cv.VideoWriter_fourcc('m', 'p', '4', 'v')
-    writer = cv.VideoWriter(v_name, fourcc, fps, (w, h))
+    fourcc = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
+    writer = cv2.VideoWriter(v_name, fourcc, fps, (w, h))
 
     for frame in frames:
         writer.write(frame)
