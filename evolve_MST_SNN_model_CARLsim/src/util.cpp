45cd0aab6e0be0711ba3859d203a91c8a580e171
Robert Bain <31903812+bainro@users.noreply.github.com>
2022-08-21 12:49:17 -0700

    notation changes mainly

--- evolve_MST_SNN_model_CARLsim/src/util.cpp
+++ evolve_MST_SNN_model_CARLsim/src/util.cpp
@@ -119,26 +119,28 @@ void shuffleTrials(int numTrials, int numTrain, int numTest, int *trainTrials, i
 
 float calcPopCorrCoef(float* x,float* y, int length)
 {
-    const float TINY=1.0e-20;//tiny number to avoide division with zero
+	//tiny number to avoide division with zero
+    const float TINY=1.0e-20;
     float res = 0;
     int j;
-    float syy=0.0,sxy=0.0,sxx=0.0,ay=0.0,ax=0.0;
+    float syy=0.0, sxy=0.0, sxx=0.0, ay=0.0, ax=0.0;
 
-    for (j=0;j<length;j++) 
-        {
+	// calculate the means
+    for (j=0; j<length; j++) {
         ax += x[j];
         ay += y[j];
     }
     ax /= length;
     ay /= length;
-    for (j=0;j<length;j++) 
-        {     
+	
+    for (j=0; j<length; j++) {     
         sxx += (x[j]-ax)*(x[j]-ax);
         syy += (y[j]-ay)*(y[j]-ay);
         sxy += (x[j]-ax)*(y[j]-ay);
     }
-    res=sxy/(sqrt(sxx*syy)+TINY);
-        return res;
+    res = sxy / (sqrt(sxx*syy) + TINY);
+    
+	return res;
 }
 
 void calcCorrCoef(float** X, float** Y, int numRow, int numCol, float* corrCoef) {
