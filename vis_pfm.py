23e1c4dd89e434f230bc524f131d5be06e9f596e
Robert Bain <31903812+bainro@users.noreply.github.com>
2022-08-22 21:41:09 -0700

    added ref to matlab equivalent fx (from paper)

--- vis_pfm.py
+++ vis_pfm.py
@@ -208,6 +208,7 @@ def speed_response(x, y, ρ_pref):
     y *= FPS
     speed_x_y = np.sqrt(x**2 + y**2)
     # @TODO ensure negative is applied correctly!
+    # https://github.com/bainro/MSTd_AE_EA/blob/main/matlab_scripts/GenerateInputStim/ModelMT.m#L25
     return np.exp(−np.log(speed_x_y + s0 / ρ_pref + s0) ** 2 / 2*σ**2) 
     
 def make_flow_csv(load_dir="./driving"):
