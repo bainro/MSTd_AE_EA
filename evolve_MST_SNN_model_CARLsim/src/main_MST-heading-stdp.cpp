76dbc1a93e2be0473a0bbacebc3df26135b794d6
Robert Bain <31903812+bainro@users.noreply.github.com>
2022-08-26 19:02:18 -0700

    trying CPU_MODE for carlsim since still hanging :/

--- evolve_MST_SNN_model_CARLsim/src/main_MST-heading-stdp.cpp
+++ evolve_MST_SNN_model_CARLsim/src/main_MST-heading-stdp.cpp
@@ -381,7 +381,7 @@ public:
 
 int main(int argc, char* argv[]) {
 	
-	const SimMode simMode = GPU_MODE;
+	const SimMode simMode = CPU_MODE; // GPU_MODE;
   	const LoggerMode verbosity = DEVELOPER; // SILENT;
   	const MSTHeadingExperiment experiment(simMode, verbosity);
 	const PTI pti(argc, argv, cout, cin);
