5a2e652035bb0cb58a47f42d6146c1ccfee91a9e
Robert Bain <31903812+bainro@users.noreply.github.com>
2022-08-28 11:59:24 -0700

    trying 2 GPUs again

--- evolve_MST_SNN_model_CARLsim/src/main_MST-heading-stdp.cpp
+++ evolve_MST_SNN_model_CARLsim/src/main_MST-heading-stdp.cpp
@@ -137,7 +137,7 @@ public:
 		// ---------------- CONFIG STATE ------------------- 
 		CARLsim* const network = new CARLsim("MST-heading", simMode, verbosity);
 		
-		gMT = network->createSpikeGeneratorGroup("MT", MTDim, EXCITATORY_POISSON, 0, GPU_CORES); //input
+		gMT = network->createSpikeGeneratorGroup("MT", MTDim, EXCITATORY_POISSON, 1, GPU_CORES); //input
 		for (unsigned int i = 0; i < numIndi; i++) {
 			
 			// creat neuron groups
