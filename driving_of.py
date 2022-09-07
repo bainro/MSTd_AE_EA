782a5fffc7665b77f89eda959e5d1ab87c9e6bed
Robert Bain <31903812+bainro@users.noreply.github.com>
2022-09-06 19:31:26 -0700

    nvr used writePFM() so removing it

--- driving_of.py
+++ driving_of.py
@@ -47,35 +47,6 @@ def readPFM(file):
     data = np.reshape(data, shape)
     data = np.flipud(data)
     return data, scale
-
-def writePFM(file, image, scale=1):
-    file = open(file, 'wb')
-
-    color = None
-
-    if image.dtype.name != 'float32':
-        raise Exception('Image dtype must be float32.')
-      
-    image = np.flipud(image)  
-
-    if len(image.shape) == 3 and image.shape[2] == 3: # color image
-        color = True
-    elif len(image.shape) == 2 or len(image.shape) == 3 and image.shape[2] == 1: # greyscale
-        color = False
-    else:
-        raise Exception('Image must have H x W x 3, H x W x 1 or H x W dimensions.')
-
-    file.write('PF\n' if color else 'Pf\n')
-    file.write('%d %d\n' % (image.shape[1], image.shape[0]))
-
-    endian = image.dtype.byteorder
-
-    if endian == '<' or endian == '=' and sys.byteorder == 'little':
-        scale = -scale
-
-    file.write('%f\n' % scale)
-
-    image.tofile(file)
     
 def img_from_fig(fig, dpi=180):
     # returns an image as numpy array from figure
