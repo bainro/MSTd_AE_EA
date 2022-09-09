4e08d192d590aa4e71c1072a3c69122d8080f3f5
Robert Bain <31903812+bainro@users.noreply.github.com>
2022-09-08 21:25:32 -0700

    Update vkitti2_of.py

--- vkitti2_of.py
+++ vkitti2_of.py
@@ -84,8 +84,8 @@ def flow_img(file="test.png", show=False):
     u[og_u < 0] = u[og_u < 0] * -1
     v[og_v < 0] = v[og_v < 0] * -1
     # arrows were flipped across both axes
-    u *= -1
-    v *= -1
+    # u *= -1
+    # v *= -1
     
     x = np.arange(0, u.shape[0], 1)
     y = np.arange(0, u.shape[1], 1)
