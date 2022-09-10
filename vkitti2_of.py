7878668008b5bfa5c182b9818f9901988482b52d
Robert Bain <31903812+bainro@users.noreply.github.com>
2022-09-09 17:44:30 -0700

    dbging why lots of flow @ stop sign

--- vkitti2_of.py
+++ vkitti2_of.py
@@ -50,6 +50,9 @@ def flow_img(file="test.png", show=False):
     
     flow = read_OF_png(file)
     h, w = flow.shape[:2]
+    print("file: ", file)
+    print("h.sum: ", h.sum())
+    print("w.sum: ", w.sum())
     new_l = round(w/2 - h/2)
     new_r = round(w/2 + h/2)
     flow = flow[:,new_l:new_r,:]
