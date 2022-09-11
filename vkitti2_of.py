eb7ffae8085e4da84c2dbf7e2589638c395c8088
Robert Bain <31903812+bainro@users.noreply.github.com>
2022-09-10 20:41:23 -0700

    Update vkitti2_of.py

--- vkitti2_of.py
+++ vkitti2_of.py
@@ -66,11 +66,12 @@ def flow_img(file="test.png", f_min=None, f_max=None, show=False):
     else:
         vmax = np.sqrt((f_min ** 2) + (f_min ** 2))
     vmax = np.log(vmax + 1)
+    cmap = plt.cm.get_cmap('hsv')
     
     axes[1].set_xticks([])
     axes[1].set_yticks([])
     axes[1].set_title("L2 Norm Optical Flow", y=1.025)
-    axes[1].imshow(a, vmin=0, vmax=vmax)
+    axes[1].imshow(a, vmin=0, vmax=vmax, cmap=cmap)
     
     h, w = flow.shape[:2]
     new_l = round(w/2 - h/2)
