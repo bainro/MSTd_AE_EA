5a2eb008c5b26b7df74a445fd4b85d472638d6aa
Robert Bain <31903812+bainro@users.noreply.github.com>
2023-02-17 20:16:55 -0800

    Update driving_of.py

--- driving_of.py
+++ driving_of.py
@@ -28,6 +28,7 @@ def csv_stats(filenames):
                 
     for filename in filenames:
         #print(f"csv_stats({filename})")    
+        import pdb; pdb.set_trace()
         min_v = 1000
         max_v = -1000
         running_total = 0       
