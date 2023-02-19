0f51935856d4989f0f79c75c59a44c44c1cb75d3
Robert Bain <31903812+bainro@users.noreply.github.com>
2023-02-18 20:37:38 -0800

    Update main_MST-heading-stdp.cpp

--- evolve_MST_SNN_model_CARLsim/src/main_MST-heading-stdp.cpp
+++ evolve_MST_SNN_model_CARLsim/src/main_MST-heading-stdp.cpp
@@ -354,7 +354,7 @@ public:
 		
 		for (unsigned int i = 0; i < numIndi; i++) {
 
-			popFitness[i] = 1 / calcRMS(recMTAll[i], testMTMatrix, numTest, nMT);
+			popFitness[i] = (1000. * 1000.) - calcRMS(recMTAll[i], testMTMatrix, numTest, nMT);
 
 			maxFR = 0.0f;
 			for (unsigned int neur = 0; neur < nMST; neur ++) {
@@ -365,10 +365,9 @@ public:
 					maxFR = sumNeurFR[i][neur];
 				}
 			}
-			// if max mean FR greater than target max FR, subtract max mean from target
-			// add penalty to fitness
+			// if max mean FR greater than target max FR, IT DIES :O
 			if (maxFR > targetMaxFR) {
-				popFitness[i] -= ((maxFR - targetMaxFR) / 1000);
+				popFitness[i] = 0;
 			}
 
 			fileFitness << "popFitness-" << i << ": " << popFitness[i] << endl;
