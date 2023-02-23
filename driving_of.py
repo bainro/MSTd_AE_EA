8dd072d6cad69412a4c68b2e7dc01d8ea87d4b60
Robert Bain <31903812+bainro@users.noreply.github.com>
2023-02-23 00:35:57 -0800

    Update driving_of.py

--- driving_of.py
+++ driving_of.py
@@ -334,6 +334,7 @@ def make_flow_csv(load_dir="./driving"):
                             _y = y[_j:(_j + win_len), _k:(_k + win_len)]
                             if θ_pref == 0:
                                 hash = imagehash.average_hash(Image.fromarray( np.uint8(np.dstack((_x,_y)) * 255) ))
+                                print(1)
                                 if prev_hash != None:
                                     print(hash - prev_hash)
                                 prev_hash = hash
