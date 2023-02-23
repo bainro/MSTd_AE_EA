268afbc554f1803c321cf8faa77c65c475f71893
Robert Bain <31903812+bainro@users.noreply.github.com>
2023-02-23 12:02:56 -0800

    Update driving_of.py

--- driving_of.py
+++ driving_of.py
@@ -356,7 +356,7 @@ def make_flow_csv(load_dir="./driving"):
     num_del = 0
     # '''
     for i, h in enumerate(hashes[::-1]):
-        if prev_hash - h < 30:
+        if prev_hash - h < 20:
             # print(prev_hash - h)
             del rows[i - num_del]
             num_del = num_del + 1
